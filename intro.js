395437c944866973dba769d1c315c2d2aecf92cf
LivinkumarS <001liv100@ggmail.com>
2024-12-28 12:19:46 +0530

    reduce function

--- intro.js
+++ intro.js
@@ -1,18 +1,27 @@
 // Array (Map,reduce,filter)
 
-let arr1 = [1,2,3,4,5,6,7,8,9,"0"];
 
 // arr1.map((item,ind) => {
-//   console.log(item);
-//   console.log(ind);
+  //   console.log(item);
+  //   console.log(ind);
 // });
 
 // filter function will return an array
 
-[]
+// const newArr= arr1.filter((item) => {
+//   return item;
+// });
 
-const newArr= arr1.filter((item) => {
-  return item;
-});
+// console.log(newArr);
 
-console.log(newArr);
+// Reduce function
+
+let arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// reduce((accumulator, item, index)=>{ return },initial value)
+
+const answer = arr1.reduce((accumulator, item) => {
+  return accumulator*item;
+}, 0);
+
+
+console.log(answer);
