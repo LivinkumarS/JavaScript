c41adc77a2d86f162a706d8235bdadafa655de82
LivinkumarS <001liv100@ggmail.com>
2024-12-31 14:04:06 +0530

    Optimized leap year code

--- intro.js
+++ intro.js
@@ -1,15 +1,12 @@
 // leap year --- divided by 4, 2000, 1904
 
-let year = prompt("Year: ");
+let year = prompt("Year: ");   //optimized code
 
-if (year % 100 == 0) {
-  if (year % 400 == 0) {
-    console.log("Leap Year");
-  } else {
-    console.log("Non leap year");
-  }
-} else if (year % 4 == 0) {
-  console.log("Leap year");
+if (
+  (year % 100 == 0 && year % 400 == 0) ||
+  (year % 100 != 0 && year % 4 == 0)
+) {
+  console.log("Leap Year");
 } else {
   console.log("Non leap year");
 }
