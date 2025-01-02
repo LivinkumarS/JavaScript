20886aae2a3de0a02086021bd2753fdd12579478
LivinkumarS <001liv100@ggmail.com>
2025-01-02 13:54:03 +0530

    Loops in JS(For, While, For In, For Of)

--- intro.js
+++ intro.js
@@ -1,12 +1,79 @@
-// leap year --- divided by 4, 2000, 1904
+// ok 100
 
-let year = prompt("Year: ");   //optimized code
+//100 time
 
-if (
-  (year % 100 == 0 && year % 400 == 0) ||
-  (year % 100 != 0 && year % 4 == 0)
-) {
-  console.log("Leap Year");
-} else {
-  console.log("Non leap year");
+//0 to 99   ++ means increment, -- means decrement
+
+let arr = [
+  "apple",
+  "orange",
+  "banana",
+  "gooseberries",
+  "blueberries",
+  "grapes",
+];
+
+let str = "hello";
+
+// console.log(arr.length);
+
+// for (let i = arr.length - 1; i >= 0; i--) {
+//   console.log(arr[i]);
+// }
+
+// console.log(arr[5]);
+// console.log(arr[4]);
+// console.log(arr[3]);
+// console.log(arr[2]);
+// console.log(arr[1]);
+// console.log(arr[0]);
+
+// while Loops
+
+// let i = 0;
+
+// while (i < 10) {
+//   console.log("less");
+//   i = i + 1;
+// }
+
+// let ans = 2; //2!=  undefined
+// let selectedAnswer; //undefined
+
+// while (true) {
+//   selectedAnswer = prompt("Type Your Answer");
+//   if (selectedAnswer == ans) {
+//     console.log("Congrats. correct answer!");
+//     break;
+//   }
+// }
+
+// For In
+
+let obj = {
+  name: "hari",
+  age: 21,
+  dob: "1 / 2 / 2003",
+};
+
+let arr1 = [
+  "apple",
+  "orange",
+  "banana",
+  "gooseberries",
+  "blueberries",
+  "grapes",
+];
+
+// console.log(arr1);
+
+// for (let keyValue in arr1) {
+//   console.log(keyValue);
+//   console.log(arr1[keyValue]);
+// }
+
+// For of
+
+for (let values of arr1) {
+  console.log(values);         // only for arrays.
 }
